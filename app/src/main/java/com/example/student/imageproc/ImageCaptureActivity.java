4ff9876d40631c5463bf8eb1b19319ddc1024f3e
Charles <cp8tk@virginia.edu>
2018-04-12 10:06:25 -0400

    Initial camera/gallery completion
    Worked on camera, gallery selections. TODO:
    
    - create another activitiy to work with image, processing code there
    
    - use Core.split for splitting the mat
    
    - make mat of bmp by using bmp to mat methods
    
    - use gaussian blur to blur images

--- app/src/main/java/com/example/student/imageproc/ImageCaptureActivity.java
+++ app/src/main/java/com/example/student/imageproc/ImageCaptureActivity.java
@@ -1,18 +1,23 @@
 package com.example.student.imageproc;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
-
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
+import org.opencv.android.Utils;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
@@ -32,6 +37,7 @@ public class ImageCaptureActivity extends AppCompatActivity implements View.OnCl
 
     Mat mat1, mat2, mat3;
     ImageView imageView;
+    Integer REQUEST_CAMERA=1, SELECT_FILE=0;
 
 
 
@@ -47,25 +53,74 @@ public class ImageCaptureActivity extends AppCompatActivity implements View.OnCl
 
     }
 
+    private void SelectImage(){
+        final CharSequence[] items = {"Camera", "Saved Images", "Cancel"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(ImageCaptureActivity.this);
+        builder.setTitle("Add Image");
+        builder.setItems(items, new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i){
+                if(items[i].equals("Camera")){
+                    //start the camera
+                    Intent startCamera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(startCamera, REQUEST_CAMERA);
+                }else if(items[i].equals("Saved Images")){
+                    //check out image gallery
+                    Intent savedImages = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    savedImages.setType("image/*");
+                    startActivityForResult(savedImages.createChooser(savedImages, "Select File"), SELECT_FILE);
+                }else if(items[i].equals("Cancel")){
+                    dialogInterface.dismiss();
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == Activity.RESULT_OK){
+
+            if(requestCode == REQUEST_CAMERA){
+                Bundle bundle =  data.getExtras();
+                final Bitmap bmp = (Bitmap) bundle.get("data");
+                imageView.setImageBitmap(bmp);
+
+                /* TO DO NEXT
+
+                to do
+                
+                //need to make bitmap into bmp32, to to another screen and process?
+                //use Core.split for splitting mat
+
+                */
+
+            }else if(requestCode == SELECT_FILE){
+                Uri selectedImageURI = data.getData();
+                imageView.setImageURI(selectedImageURI);
+            }
+        }
+
+    }
+
+
+
+
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.takePicture:
                 //stuff
                 //Imgcodecs.imwrite()
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent, 0);
-
+                //use another function for when image taken
+                //Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                //startActivityForResult(intent, 0);
+                SelectImage();
                 break;
         }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        super.onActivityResult(requestCode, resultCode, data);
-        Bitmap bitmap = (Bitmap)data.getExtras().get("data");
-        imageView.setImageBitmap(bitmap);
 
-    }
 
 }
